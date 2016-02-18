1eb6860450b3c881e77368d8d3141ec4b590ef9c
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-02-18 10:36:09 +0100

    update

--- test/xml-key-discovery.js
+++ test/xml-key-discovery.js
@@ -7,70 +7,241 @@ var assert = require('chai').assert,
   XMLKeyDiscovery = discovery.XMLKeyDiscovery;
 
 describe('XMLKeyDiscovery', function () {
-  describe('basic test', function () {
-    it('execute', function () {
-      var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
-        '<bookstore>' +
-        '<book category="COOKING" haha="h">' +
-        '<title lang="en">Everyday Italian</title>' +
-        '<id>0</id>' +
-        '<test>fdfd</test>' +
-        '<author>Giada De Laurentiis</author>' +
-        '<year>2005</year>' +
-        '<price>30.00</price>' +
-        '</book>' +
-        '<book category="COOKING2">' +
-        '<title lang="en">Everyday Italian2</title>' +
-        '<id>1</id>' +
-        '<author>Giada</author>' +
-        '<year>2025</year>' +
-        '<price>31.00</price>' +
-        '</book>' +
-        '<book category="CHILDREN">' +
-        '<title lang="en">Harry Potter</title>' +
-        '<id>2</id>' +
-        '<author>J K. Rowling</author>' +
-        '<year>2005</year>' +
-        '<price>29.99</price>' +
-        '</book>' +
-        '<book category="WEB">' +
-        '<title lang="en">XQuery Kick Start</title>' +
-        '<id>3</id>' +
-        '<author>James McGovern</author>' +
-        '<author>Per Bothner</author>' +
-        '<author>Kurt Cagle</author>' +
-        '<author>James Linn</author>' +
-        '<author>Vaidyanathan Nagarajan</author>' +
-        '<year>2003</year>' +
-        '<price>49.99</price>' +
-        '</book>' +
-        '<book category="WEB">' +
-        '<title lang="en">Learning XML</title>' +
-        '<id>4</id>' +
-        '<author>Erik T. Ray</author>' +
-        '<year>2003</year>' +
-        '<price>39.95</price>' +
-        '</book>' +
-        '</bookstore>';
+  it('#1', function () {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      '<bookstore>' +
+      '<book>' +
+      '<title>Everyday Italian</title>' +
+      '<id>0</id>' +
+      '<author>Giada De Laurentiis</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>Everyday Italian2</title>' +
+      '<id>1</id>' +
+      '<author>Giada</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>Harry Potter</title>' +
+      '<id>2</id>' +
+      '<author>J K. Rowling</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>XQuery Kick Start</title>' +
+      '<id>3</id>' +
+      '<author>James McGovern</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>Learning XML</title>' +
+      '<id>4</id>' +
+      '<author>Erik T. Ray</author>' +
+      '</book>' +
+      '</bookstore>';
 
-      var nodePath = "/bookstore/book";
+    var nodePath = "/bookstore/book";
 
-      var discovery = new XMLKeyDiscovery(xml);
-      var result = discovery.discover(nodePath);
-      var expectedResult = [['title'],
-        ['id'],
-        ['price'],
-        ['@category', 'title'],
-        ['@category', 'id'],
-        ['@category', 'price'],
-        ['year', 'title'],
-        ['year', 'id'],
-        ['year', 'price'],
-        ['year', '@category', 'title'],
-        ['year', '@category', 'id'],
-        ['year', '@category', 'price']];
+    var discovery = new XMLKeyDiscovery(xml);
+    var result = discovery.discover(nodePath);
+    var expectedResult = [['title'],
+      ['id'],
+      ['author']];
 
-      assert.deepEqual(result, expectedResult, "correct number of keys not found");
-    });
+    assert.deepEqual(result, expectedResult, "correct number of keys not found");
+  });
+
+  it('#2', function () {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      '<bookstore>' +
+      '<book id="0">' +
+      '<title>Everyday Italian</title>' +
+      '<author>Giada De Laurentiis</author>' +
+      '</book>' +
+      '<book id="1">' +
+      '<title>Everyday Italian2</title>' +
+      '<author>Giada</author>' +
+      '</book>' +
+      '<book id="2">' +
+      '<title>Harry Potter</title>' +
+      '<author>J K. Rowling</author>' +
+      '</book>' +
+      '<book id="3">' +
+      '<title>XQuery Kick Start</title>' +
+      '<author>James McGovern</author>' +
+      '</book>' +
+      '</bookstore>';
+
+    var nodePath = "/bookstore/book";
+
+    var discovery = new XMLKeyDiscovery(xml);
+    var result = discovery.discover(nodePath);
+    var expectedResult = [['@id'], ['title'],
+      ['author']];
+
+    //console.log(result);
+
+    assert.deepEqual(result, expectedResult, "correct number of keys not found");
+  });
+
+  it('#3', function () {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      '<bookstore>' +
+      '<book id="0">' +
+      '<title>Everyday Italian</title>' +
+      '<author>Giada De Laurentiis</author>' +
+      '</book>' +
+      '<book id="1">' +
+      '<title>Everyday Italian2</title>' +
+      '<author>Giada</author>' +
+      '</book>' +
+      '<book id="1">' +
+      '<title>Harry Potter</title>' +
+      '<author>J K. Rowling</author>' +
+      '</book>' +
+      '<book id="3">' +
+      '<title>XQuery Kick Start</title>' +
+      '<author>James McGovern</author>' +
+      '</book>' +
+      '</bookstore>';
+
+    var nodePath = "/bookstore/book";
+
+    var discovery = new XMLKeyDiscovery(xml);
+    var result = discovery.discover(nodePath);
+    var expectedResult = [['title'], ['author'],
+      ['@id', 'title'], ['@id', 'author']];
+
+    //console.log(result);
+
+    assert.deepEqual(result, expectedResult, "correct number of keys not found");
+  });
+
+  it('#4', function () {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      '<bookstore>' +
+      '<book id="0">' +
+      '<title>Everyday Italian</title>' +
+      '<author>Giada De Laurentiis</author>' +
+      '</book>' +
+      '<book id="1">' +
+      '<title>Everyday Italian2</title>' +
+      '<author>Giada</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>Harry Potter</title>' +
+      '<author>J K. Rowling</author>' +
+      '</book>' +
+      '<book id="3">' +
+      '<title>XQuery Kick Start</title>' +
+      '<author>James McGovern</author>' +
+      '</book>' +
+      '</bookstore>';
+
+    var nodePath = "/bookstore/book";
+
+    var discovery = new XMLKeyDiscovery(xml);
+    var result = discovery.discover(nodePath);
+    var expectedResult = [['@id'], ['title'],
+      ['author']];
+
+    //console.log(result);
+
+    assert.deepEqual(result, expectedResult, "correct number of keys not found");
+  });
+
+  it('#5', function () {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      '<bookstore>' +
+      '<book id="0">' +
+      '<title>Everyday Italian</title>' +
+      '<author>Giada De Laurentiis</author>' +
+      '</book>' +
+      '<book id="1">' +
+      '<title>Everyday Italian2</title>' +
+      '<author>Giada</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>Harry Potter</title>' +
+      '<author>J K. Rowling</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>XQuery Kick Start</title>' +
+      '<author>James McGovern</author>' +
+      '</book>' +
+      '</bookstore>';
+
+    var nodePath = "/bookstore/book";
+
+    var discovery = new XMLKeyDiscovery(xml);
+    var result = discovery.discover(nodePath);
+    var expectedResult = [['title'],
+      ['author'],
+      ['@id', 'title'],
+      ['@id', 'author']];
+
+    //console.log(result);
+
+    assert.deepEqual(result, expectedResult, "correct number of keys not found");
+  });
+
+  it('#6', function () {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      '<bookstore>' +
+      '<book>' +
+      '<title>Everyday Italian</title>' +
+      '</book>' +
+      '<book>' +
+      '<title>Everyday Italian2</title>' +
+      '<author>Giada</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>Harry Potter</title>' +
+      '<author>J K. Rowling</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>XQuery Kick Start</title>' +
+      '<author>James McGovern</author>' +
+      '</book>' +
+      '</bookstore>';
+
+    var nodePath = "/bookstore/book";
+
+    var discovery = new XMLKeyDiscovery(xml);
+    var result = discovery.discover(nodePath);
+    var expectedResult = [['title'],
+      ['author']];
+
+    //console.log(result);
+
+    assert.deepEqual(result, expectedResult, "correct number of keys not found");
+  });
+
+  it('#7', function () {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
+      '<bookstore>' +
+      '<book>' +
+      '<title>Everyday Italian</title>' +
+      '</book>' +
+      '<book>' +
+      '<title>Everyday Italian2</title>' +
+      '</book>' +
+      '<book>' +
+      '<title>Harry Potter</title>' +
+      '<author>J K. Rowling</author>' +
+      '</book>' +
+      '<book>' +
+      '<title>XQuery Kick Start</title>' +
+      '<author>James McGovern</author>' +
+      '</book>' +
+      '</bookstore>';
+
+    var nodePath = "/bookstore/book";
+
+    var discovery = new XMLKeyDiscovery(xml);
+    var result = discovery.discover(nodePath);
+    var expectedResult = [['title'],
+      ['author', 'title']];
+
+    //console.log(result);
+
+    assert.deepEqual(result, expectedResult, "correct number of keys not found");
   });
 });
